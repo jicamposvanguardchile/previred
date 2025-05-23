9a814bc170d8dd63a433c0b492efb11788cd0a78
unknown <matias.marchant.u@gmail.com>
2025-05-23 17:47:58 -0400

    cambio nombre

--- l10n_cl_hr/model/hr_payslip_input_type.py
+++ l10n_cl_hr/model/hr_payslip_input_type.py
@@ -7,30 +7,15 @@ import logging
 _logger = logging.getLogger(__name__)
 
 
-class HrPayslipInputType(models.Model):
-    _inherit = 'hr.payslip.input.type'
+class CLHrPayslipInputType(models.Model):
+    _name = 'l10n_cl_hr.payslip.input.type'
+    _description = 'Tipo de input para nómina chilena'
 
+    name = fields.Char(string="Nombre", required=True)
+    code = fields.Char(string="Código")
     show_input = fields.Boolean('Agrega input en Nómina', default=False)
     invisible_show_input = fields.Boolean(compute='_compute_invisible_show_input')
 
-#    @api.depends('show_input')
     def _compute_invisible_show_input(self):
-        attachment_types = self._get_attachment_types()
-        attachment_type_ids = [f.id for f in attachment_types.values()]
         for input_type in self:
-            _logger.info(' CCCC ')
-            _logger.info(attachment_types)
-            _logger.info(attachment_type_ids)
-            if input_type.id in attachment_type_ids:
-                input_type.invisible_show_input = True
-            else:
-                input_type.invisible_show_input = False
-
-    # Analogo al existente en hr_payslip
-    @api.model
-    def _get_attachment_types(self):
-        return {
-            'attachment': self.env.ref('pways_hr_payroll.input_attachment_salary'),
-            'assignment': self.env.ref('pways_hr_payroll.input_assignment_salary'),
-            'child_support': self.env.ref('pways_hr_payroll.input_child_support'),
-        }
+            input_type.invisible_show_input = not input_type.show_input
