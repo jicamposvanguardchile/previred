cd3f230191b3e07b97200c98b651e8eeed936c45
unknown <matias.marchant.u@gmail.com>
2025-05-23 19:41:17 -0400

    comentado

--- l10n_cl_hr/model/hr_payslip_input_type.py
+++ l10n_cl_hr/model/hr_payslip_input_type.py
@@ -7,30 +7,29 @@ import logging
 _logger = logging.getLogger(__name__)
 
 
-class HrPayslipInputType(models.Model):
-    _inherit = 'hr.payslip.input.type'
+#class HrPayslipInputType(models.Model):
+#    _inherit = 'hr.payslip.input.type'
 
-    show_input = fields.Boolean('Agrega input en Nómina', default=False)
-    invisible_show_input = fields.Boolean(compute='_compute_invisible_show_input')
+#    show_input = fields.Boolean('Agrega input en Nómina', default=False)
+ #   invisible_show_input = fields.Boolean(compute='_compute_invisible_show_input')
 
 #    @api.depends('show_input')
-    def _compute_invisible_show_input(self):
-        attachment_types = self._get_attachment_types()
-        attachment_type_ids = [f.id for f in attachment_types.values()]
-        for input_type in self:
-            _logger.info(' CCCC ')
-            _logger.info(attachment_types)
-            _logger.info(attachment_type_ids)
-            if input_type.id in attachment_type_ids:
-                input_type.invisible_show_input = True
-            else:
-                input_type.invisible_show_input = False
+  #  def _compute_invisible_show_input(self):
+  #      attachment_types = self._get_attachment_types()
+   #     attachment_type_ids = [f.id for f in attachment_types.values()]
+    #    for input_type in self:
+     #       _logger.info(' CCCC ')
+      ##     _logger.info(attachment_type_ids)
+        #    if input_type.id in attachment_type_ids:
+         #       input_type.invisible_show_input = True
+          #  else:
+           #     input_type.invisible_show_input = False
 
     # Analogo al existente en hr_payslip
-    @api.model
-    def _get_attachment_types(self):
-        return {
-            'attachment': self.env.ref('hr_payroll.input_attachment_salary'),
-            'assignment': self.env.ref('hr_payroll.input_assignment_salary'),
-            'child_support': self.env.ref('hr_payroll.input_child_support'),
-        }
+    #@api.model
+    #def _get_attachment_types(self):
+    #    return {
+     #       'attachment': self.env.ref('hr_payroll.input_attachment_salary'),
+      #      'assignment': self.env.ref('hr_payroll.input_assignment_salary'),
+       #     'child_support': self.env.ref('hr_payroll.input_child_support'),
+        #}
