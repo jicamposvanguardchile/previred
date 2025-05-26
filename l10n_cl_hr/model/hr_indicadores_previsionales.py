c34d57dd2cfe6b84aaecedd566d8b00337f96fa6
unknown <matias.marchant.u@gmail.com>
2025-05-26 17:03:02 -0400

    cambio nombre

--- l10n_cl_hr/model/hr_indicadores_previsionales.py
+++ l10n_cl_hr/model/hr_indicadores_previsionales.py
@@ -255,7 +255,7 @@ class hr_indicadores_previsionales(models.Model):
             'UF':{},
             'UTM':0.0,
             'UTA':[],
-            'MES_UTM':'',
+            'MES_UTM':[],
             'RENTAS_TOPE_AFP':[],
             'RENTAS_TOPE_IPS':[],
             'RENTAS_TOPE_SEGURO':[],
