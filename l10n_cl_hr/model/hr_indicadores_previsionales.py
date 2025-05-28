55891c269d9ee8cf332176fa456ed183b1630828
unknown <matias.marchant.u@gmail.com>
2025-05-28 17:05:57 -0400

    cambio nombre

--- l10n_cl_hr/model/hr_indicadores_previsionales.py
+++ l10n_cl_hr/model/hr_indicadores_previsionales.py
@@ -513,5 +513,6 @@ class hr_indicadores_previsionales(models.Model):
                                 indicadores['COTIZACION_TRAB_PESADO'] = [empleador, trabajador]
                 
         self.env['hr.indicadores2'].create(indicadores)
+        _logger.info('self %s'%(self))
 
         return indicadores
