bf28b7682abf5ab01582f8995443623e6ccfd4eb
unknown <matias.marchant.u@gmail.com>
2025-05-30 15:48:21 -0400

    cambio nombre

--- l10n_cl_hr/model/hr_indicadores_previsionales.py
+++ l10n_cl_hr/model/hr_indicadores_previsionales.py
@@ -323,10 +323,10 @@ class hr_indicadores_previsionales(models.Model):
                 texto = normalizar(texto_raw)
 
                  # UF
-            if ('uf abril' in texto or '30 de abril del' in texto) and indicadores['UF'].get('ABRIL'):
+            if ('uf abril' in texto or '30 de abril del' in texto):
                 indicadores['UF']['ABRIL'] = extraer_monto(texto_raw)
 
-            if ('uf mayo' in texto or '31 de mayo del' in texto) and indicadores['UF'].get('MAYO'):
+            if ('uf mayo' in texto or '31 de mayo del' in texto):
                 indicadores['UF']['MAYO'] = extraer_monto(texto_raw)
 
             # UTM y UTA
