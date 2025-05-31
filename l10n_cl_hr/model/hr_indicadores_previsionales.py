733b98f8986c29521fc6d1dd5fd2adf16c0a5e2e
unknown <matias.marchant.u@gmail.com>
2025-05-31 17:51:09 -0400

    cambio nombre

--- l10n_cl_hr/model/hr_indicadores_previsionales.py
+++ l10n_cl_hr/model/hr_indicadores_previsionales.py
@@ -416,7 +416,7 @@ class hr_indicadores_previsionales(models.Model):
                         ]
                 
                 # Tasa AFP Cuprum
-                if 'curpum' in texto and indicadores ['TASA_CUPRUM'] == 0:
+                if 'cuprum' in texto and not indicadores['TASA_CUPRUM']:
                     celdas = fila.find_all('td')
                     if len(celdas) >= 4:
                         indicadores['TASA_CUPRUM'] = [
