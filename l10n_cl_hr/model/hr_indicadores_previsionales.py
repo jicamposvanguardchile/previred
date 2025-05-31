709902f1159ccc47008499755574d9a9dddb6633
unknown <matias.marchant.u@gmail.com>
2025-05-31 18:14:15 -0400

    cambio nombre

--- l10n_cl_hr/model/hr_indicadores_previsionales.py
+++ l10n_cl_hr/model/hr_indicadores_previsionales.py
@@ -444,6 +444,16 @@ class hr_indicadores_previsionales(models.Model):
                             extraer_monto(celdas[2].get_text(strip=True)),
                             extraer_monto(celdas[3].get_text(strip=True))
                         ]
+                
+                # Tasa AFP ProVida
+                if 'provida' in texto and indicadores['TASA_PROVIDA'] == 0:
+                    celdas = fila.find_all('td')
+                    if len(celdas) >= 4:
+                        indicadores['TASA_PROVIDA'] = [
+                            extraer_monto(celdas[1].get_text(strip=True)),
+                            extraer_monto(celdas[2].get_text(strip=True)),
+                            extraer_monto(celdas[3].get_text(strip=True))
+                        ]
                                 
                 # Asignacion Familiar
                 if '1 (a)' in texto and indicadores['ASIGNACION_FAMILIAR_A'] == 0:
