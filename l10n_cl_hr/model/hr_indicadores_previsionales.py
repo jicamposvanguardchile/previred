df168f52c5124fe2985f339a0635d04a0cf3c6a6
unknown <matias.marchant.u@gmail.com>
2025-06-02 16:46:32 -0400

    cambio nombre

--- l10n_cl_hr/model/hr_indicadores_previsionales.py
+++ l10n_cl_hr/model/hr_indicadores_previsionales.py
@@ -389,7 +389,7 @@ class hr_indicadores_previsionales(models.Model):
 
                 #plazo indefinido 11 años
                 # Seguro Cesantía 11 años
-                if 'plazo indefinido 11 años o mas (*)' in texto and not indicadores['SEGURO_CESANTIA_11_ANNOS'] == 0:
+                if '11 años o mas' in texto and not indicadores['SEGURO_CESANTIA_11_ANNOS'] == 0:
                     celdas = fila.find_all('td')
                     if len(celdas) >= 2:
                         indicadores['SEGURO_CESANTIA_11_ANNOS'] = [
