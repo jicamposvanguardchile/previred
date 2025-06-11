767789efb63705f4696137a9c7a35403b5df510e
unknown <matias.marchant.u@gmail.com>
2025-06-11 19:46:13 -0400

    ipc

--- l10n_cl_hr/model/hr_indicadores_previsionales.py
+++ l10n_cl_hr/model/hr_indicadores_previsionales.py
@@ -612,7 +612,7 @@ class hr_indicadores_previsionales(models.Model):
                 texto_raw_ipc = fila_ipc.get_text(strip=True)
                 textos = normalizar(texto_raw_ipc)
 
-                if '2025' in textos and indicadores['IPC'] == 0:
+                if '2025' in textos and 'gr_ctl99_Mayo' and indicadores['IPC'] == 0:
                     celdas = fila_ipc.find_all("td")
                     if len(celdas) >= 6:
                         texto_ipc = celdas[5].get_text(strip=True)
